3e836672b371f0be941665d05da4134b74a0be33
ACharLuk <acharluk@gmail.com>
2017-08-30 13:31:13 +0200

    Linux support!

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+// import * as os from 'os';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -11,18 +12,26 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('Luna-vscode extension is now active!');
 
+    let term, args, argsmain;
+
+    if (process.platform === 'win32') {
+        term = 'cmd.exe';
+        args = [ '/Q', '/C', 'luna', vscode.window.activeTextEditor.document.fileName, '&&', 'pause' ];
+        argsmain = [ '/Q', '/C', 'luna', vscode.workspace.rootPath + '/main.luna', '&&', 'pause'];
+    } else if (process.platform === 'linux') {
+        term = 'bash';
+        args = [ '-c', 'luna', vscode.window.activeTextEditor.document.fileName, '&&', 'read'];
+        args = [ '-c', 'luna', vscode.workspace.rootPath + '/main.luna', '&&', 'read'];
+    }
+
     let luna_run_current = vscode.commands.registerCommand('luna.run.current', () => {
-        let x = vscode.window.createTerminal('Luna terminal', 'cmd.exe',
-        [ '/Q', '/C', 'luna', vscode.window.activeTextEditor.document.fileName, '&&', 'pause' ]);
+        let x = vscode.window.createTerminal('Luna terminal', term, args);
         x.show();
-        
     });
 
     let luna_run_main = vscode.commands.registerCommand('luna.run.main', () => {
-        let x = vscode.window.createTerminal('Luna terminal', 'cmd.exe',
-        [ '/Q', '/C', 'luna', vscode.workspace.rootPath + '/main.luna', '&&', 'pause']);
+        let x = vscode.window.createTerminal('Luna terminal', term, argsmain);
         x.show();
-        
     });
 
     context.subscriptions.push(luna_run_current);
