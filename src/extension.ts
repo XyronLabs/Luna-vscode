4b9692875bf99d6cee7db9df8969dbc8492b739e
ACharLuk <acharluk@gmail.com>
2017-09-04 17:35:36 +0200

    Testint automatic download of Luna

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-// import * as os from 'os';
+import * as fs from 'fs';
+import * as request from 'request';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -33,6 +34,16 @@ export function activate(context: vscode.ExtensionContext) {
         let x = vscode.window.createTerminal('Luna terminal', term, argsmain);
         x.show();
     });
+
+    request.get({url: 'https://github.com/XyronLabs/Luna/releases/download/0.7-12/luna-0.7-12_setup.exe', encoding: 'binary'}, function (err, response, body) {
+        fs.writeFile( __dirname + "/setup.exe", body, 'binary', function(err) {
+          if(err)
+            console.log(err);
+          else
+            console.log("Latest Luna setup downloaded!");
+        }); 
+      });
+    
 
     context.subscriptions.push(luna_run_current);
     context.subscriptions.push(luna_run_main);
