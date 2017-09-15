ca51e8753be2cf71fcea7ef2009d10d8fd3d9117
ACharLuk <acharluk@gmail.com>
2017-09-15 02:20:04 +0200

    Use output channel and fix check update

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,25 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as request from 'request';
 import * as extract_zip from 'extract-zip';
 
-let luna_version;
+let luna_version, luna_output;
 let term, args1, args2, argsmain;
-let luna_output;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    vscode.window.showInformationMessage('Luna is checking for updates, please wait...');
-    checkLunaInstalled();
+    // Initialize output channel
+    luna_output = vscode.window.createOutputChannel('Luna');
+    
+    luna_output.appendLine('Luna is checking for updates, please wait...');
+    luna_output.show();
 
     // Latest Luna release (from Debian control file)
     request.get({url: 'https://raw.githubusercontent.com/XyronLabs/Luna/master/build/debian/control'}, (err, response, body) => {
         let text :String = body;
         luna_version = text.match("[0-9].[0-9](.[0-9])?-[0-9][0-9]([0-9])?")[0];
+        checkLunaInstalled();
     });
 
     if (process.platform === 'win32') {
@@ -38,13 +35,13 @@ export function activate(context: vscode.ExtensionContext) {
     }
 
     let luna_run_current = vscode.commands.registerCommand('luna.run.current', () => {
-        vscode.window.showInformationMessage("Launching Luna: " + vscode.window.activeTextEditor.document.fileName);
+        luna_output.appendLine("Launching Luna: " + vscode.window.activeTextEditor.document.fileName);
         let x = vscode.window.createTerminal('Luna terminal', term, [args1 , vscode.window.activeTextEditor.document.fileName, args2]);
         x.show();
     });
 
     let luna_run_main = vscode.commands.registerCommand('luna.run.main', () => {
-        vscode.window.showInformationMessage("Launching Luna: " + vscode.workspace.rootPath + '/main.luna');
+        luna_output.appendLine("Launching Luna: " + vscode.workspace.rootPath + '/main.luna');
         let x = vscode.window.createTerminal('Luna terminal', term, argsmain);
         x.show();
     });
@@ -62,20 +59,20 @@ function checkLunaInstalled() {
     if (!ver || ver < luna_version) {
         installLuna();
     } else {
-        vscode.window.showInformationMessage('Luna is up to date!');
+        luna_output.appendLine('Luna is up to date!');
     }
 }
 
 function installLuna() {
     // Install latest Luna version
-    vscode.window.showInformationMessage("Installing Luna " + luna_version + " to this folder: " + vscode.workspace.rootPath);
+    luna_output.appendLine("Installing Luna " + luna_version + " to this folder: " + vscode.workspace.rootPath);
     request.get({url: 'https://github.com/XyronLabs/Luna/releases/download/' + luna_version + '/luna-' + luna_version + '_standalone_' + process.platform + '.zip', encoding: null}, (err, response, body) => {
         fs.writeFileSync(vscode.workspace.rootPath + "/luna.zip", body, 'binary');
 
         extract_zip(vscode.workspace.rootPath + "/luna.zip", {dir: vscode.workspace.rootPath}, (err) => {
             if (err) vscode.window.showErrorMessage(err);
             fs.unlinkSync(vscode.workspace.rootPath + "/luna.zip");
-            vscode.window.showInformationMessage("Luna was successfully updated!");
+            luna_output.appendLine("Luna was successfully updated!");
         });
     });
 
