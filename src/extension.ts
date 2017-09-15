b35bca8938334cca54c6850d422da39ea6c3a86a
ACharLuk <acharluk@gmail.com>
2017-09-15 02:06:10 +0200

    Auto updates

--- src/extension.ts
+++ src/extension.ts
@@ -6,8 +6,9 @@ import * as fs from 'fs';
 import * as request from 'request';
 import * as extract_zip from 'extract-zip';
 
-let luna_version = '0.7.1-13' // TODO: Check from remote
+let luna_version;
 let term, args1, args2, argsmain;
+let luna_output;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -17,6 +18,12 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     vscode.window.showInformationMessage('Luna is checking for updates, please wait...');
     checkLunaInstalled();
+
+    // Latest Luna release (from Debian control file)
+    request.get({url: 'https://raw.githubusercontent.com/XyronLabs/Luna/master/build/debian/control'}, (err, response, body) => {
+        let text :String = body;
+        luna_version = text.match("[0-9].[0-9](.[0-9])?-[0-9][0-9]([0-9])?")[0];
+    });
 
     if (process.platform === 'win32') {
         term = 'cmd.exe';
@@ -44,12 +51,6 @@ export function activate(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(luna_run_current);
     context.subscriptions.push(luna_run_main);
-
-    if (process.platform === 'win32') {
-        context.subscriptions.push(vscode.commands.registerCommand('luna.download', () => {
-            installLuna();
-        }));
-    }
 }
 
 // this method is called when your extension is deactivated
@@ -57,7 +58,8 @@ export function deactivate() {
 }
 
 function checkLunaInstalled() {
-    if (!vscode.workspace.getConfiguration('luna').get('version')) {
+    let ver = vscode.workspace.getConfiguration('luna').get('version');
+    if (!ver || ver < luna_version) {
         installLuna();
     } else {
         vscode.window.showInformationMessage('Luna is up to date!');
