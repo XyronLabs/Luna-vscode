c7d92db27d52bc7d56dae10c30ccf3fac4d39799
ACharLuk <acharluk@gmail.com>
2018-01-24 11:29:14 +0100

    Let's start refractoring!

---
+++ src/LunaProject.ts
@@ -0,0 +1,32 @@
+import { OutputChannel, Terminal, StatusBarItem } from 'vscode';
+
+export class LunaProject {
+
+    // binariesVersion: string;
+    outputChannel: OutputChannel;
+    terminal: Terminal;
+
+    buttonLaunch: StatusBarItem;
+    buttonOpenWiki: StatusBarItem;
+    buttonOpenOutput: StatusBarItem;
+
+    constructor() {
+
+    }
+
+    dispose() {
+
+    }
+
+    launch() {
+
+    }
+
+    checkForUpdates() {
+
+    }
+
+    updateBinaries() {
+
+    }
+}
