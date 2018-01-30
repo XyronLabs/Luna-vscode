3c12fb9164200dcac7f28472cf748ff0e0d3cb0e
ACharLuk <acharluk@gmail.com>
2018-01-30 18:12:38 +0100

    Add LunaExtension interface, move all code to method

--- src/ExtensionHandler.ts
+++ src/ExtensionHandler.ts
@@ -1,36 +1,55 @@
 import * as request from 'request';
 import * as fs from 'fs';
-import { workspace, ExtensionContext, commands, window } from 'vscode';
+import { workspace, ExtensionContext, commands, window, InputBoxOptions } from 'vscode';
+
+interface LunaExtension {
+    name: string;
+    description: string;
+    version: string;
+    files: string[];
+}
 
 export default class ExtensionHandler {
 
+    private baseUrl: string;
+    private extensionFolder: string;
+
     constructor(context: ExtensionContext) {
+        this.baseUrl = "https://raw.githubusercontent.com/XyronLabs/Luna-extensions/master/";
+        this.extensionFolder = workspace.rootPath + "/res/lua/extensions/";
+
         this.registerCommands(context);
     }
 
-    installExtension(packageName: string) {
-        request.get({url: "https://raw.githubusercontent.com/XyronLabs/Luna-extensions/master/" + packageName + "/extension.json", encoding: 'binary'}, (err, response, body) => {
-            let obj = JSON.parse(body);
-            obj.files.push("init.lua");
-            obj.files.push("extension.json");
+    installExtension() {
+        request.get({url: this.baseUrl + "luna_extensions.json"}, (err, response, body) => {
+            let options = JSON.parse(body);
+               
+            window.showQuickPick(options).then(packageName => {
+                if (packageName) {
+                    request.get({url: this.baseUrl + packageName + "/extension.json"}, (err, response, body) => {
+                        let obj: LunaExtension = JSON.parse(body);
+                        obj.files.push("init.lua");
+                        obj.files.push("extension.json");
+            
+                        if (!fs.existsSync(this.extensionFolder + packageName + "/"))
+                            fs.mkdirSync(this.extensionFolder + packageName + "/");
 
-            if (!fs.existsSync(workspace.rootPath + "/res/lua/extensions/" + packageName + "/"))
-                fs.mkdirSync(workspace.rootPath + "/res/lua/extensions/" + packageName + "/");
-
-            for(let f of obj.files) {
-                request.get({url: "https://raw.githubusercontent.com/XyronLabs/Luna-extensions/master/" + packageName + "/" + f, encoding: 'binary'}, (err, response, body) => {
-                    fs.writeFileSync(workspace.rootPath + "/res/lua/extensions/" + packageName + "/" + f, body, 'binary');
-                });
-            }
+                        for(let f of obj.files) {
+                            request.get({url: this.baseUrl + packageName + "/" + f}, (err, response, body) => {
+                                fs.writeFileSync(this.extensionFolder + packageName + "/" + f, body);
+                            });
+                        }
+                    });
+                }
+            });
+            
         });
     }
 
     private registerCommands(context: ExtensionContext): void {
-        let install_extension = commands.registerCommand('luna.install.extension', () => {
-            window.showInputBox().then(packageName => {
-                this.installExtension(packageName);
-            });
-        });
+        let install_extension = commands.registerCommand('luna.install.extension', () => this.installExtension());
+
         context.subscriptions.push(install_extension);
     }
 }
