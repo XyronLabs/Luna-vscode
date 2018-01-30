1e313b8e014828ff7d51e801220bf7c9581d9313
ACharLuk <acharluk@gmail.com>
2018-01-30 20:11:06 +0100

    Add abstractions

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,12 +1,25 @@
-import { OutputChannel, window } from "vscode";
+import { OutputChannel, window, workspace } from "vscode";
 
 
 export default class Logger {
     private static outputChannel: OutputChannel = window.createOutputChannel('Luna');
     private static dateTime: Date = new Date();
+    public static autoHideOutput: boolean = workspace.getConfiguration('luna').get("autoHideOutput");
 
     static println(text: string): void {
         this.dateTime.setTime(Date.now());
         this.outputChannel.appendLine("[" + this.dateTime.toLocaleString() + "] " + text);
+    }
+
+    static show(): void {
+        this.outputChannel.show();
+    }
+
+    static hide(): void {
+        this.outputChannel.hide();
+    }
+
+    static dispose(): void {
+        if (this.outputChannel) this.outputChannel.dispose();
     }
 }
