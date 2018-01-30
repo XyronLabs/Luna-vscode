8bec66f5877c3560382b0c7db9284e1263427aa8
ACharLuk <acharluk@gmail.com>
2018-01-30 20:11:19 +0100

    Use Logger class

--- src/LunaProject.ts
+++ src/LunaProject.ts
@@ -5,12 +5,9 @@ import * as extract_zip from 'extract-zip';
 
 import LaunchHandler from './LaunchHandler';
 import ExtensionHandler from './ExtensionHandler';
+import Logger from './Logger';
 
 export default class LunaProject {
-
-    public outputChannel: vscode.OutputChannel;
-    private autoHideOutput: boolean;
-    private dateTime: Date;
 
     private buttonLaunch: vscode.StatusBarItem;
     private buttonOpenWiki: vscode.StatusBarItem;
@@ -20,9 +17,6 @@ export default class LunaProject {
     private extensionHandler: ExtensionHandler;
 
     constructor(context: vscode.ExtensionContext) {
-        this.outputChannel = vscode.window.createOutputChannel('Luna');
-        this.autoHideOutput = vscode.workspace.getConfiguration('luna').get("autoHideOutput");
-        this.dateTime = new Date();
         
         this.registerCommands(context);
         this.initializeButtons();
@@ -40,15 +34,15 @@ export default class LunaProject {
     }
     
     dispose(): void {
-        if (this.outputChannel) this.outputChannel.dispose();
         if (this.buttonLaunch) this.buttonLaunch.dispose();
         if (this.buttonOpenWiki) this.buttonOpenWiki.dispose();
         if (this.buttonOpenOutput) this.buttonOpenOutput.dispose();
         this.launchHandler.dispose();
+        Logger.dispose();
     }
     
     launch(fileName: string): void {
-        this.println("Launching Luna: " + fileName);
+        Logger.println("Launching Luna: " + fileName);
         this.launchHandler.launch(fileName);
     }
 
@@ -65,48 +59,48 @@ export default class LunaProject {
     }
 
     checkForUpdates(force?: boolean, autoHide?: boolean): void {
-        this.outputChannel.show();
-        this.println("Luna is checking for updates, please wait...");
+        Logger.show();
+        Logger.println("Luna is checking for updates, please wait...");
         let currentVersion = this.checkCurrentBinariesVersion();
         
         this.checkRemoteBinariesVersion(remoteVersion => {
-            this.println("Current version: " + currentVersion);
-            this.println("Remote version: " + remoteVersion);
+            Logger.println("Current version: " + currentVersion);
+            Logger.println("Remote version: " + remoteVersion);
     
             if (!remoteVersion) {
-                this.println("Error fetching the latest version!");
+                Logger.println("Error fetching the latest version!");
                 return;
             }
     
             if (!currentVersion || currentVersion < remoteVersion || force)
                 this.updateBinaries(remoteVersion);
             else
-                this.println('Luna is up to date!\n');
+                Logger.println('Luna is up to date!\n');
             
-            if (this.autoHideOutput && autoHide)
-                this.outputChannel.hide();
+            if (Logger.autoHideOutput && autoHide)
+                Logger.hide();
         });
     }
 
     updateBinaries(remoteVersion: string): void {
-        this.println("Installing Luna " + remoteVersion + " to this folder: " + vscode.workspace.rootPath);
-        this.println("Please wait until this process is finished...")
+        Logger.println("Installing Luna " + remoteVersion + " to this folder: " + vscode.workspace.rootPath);
+        Logger.println("Please wait until this process is finished...")
         
         let url = 'https://github.com/XyronLabs/Luna/releases/download/' + remoteVersion + '/luna-' + remoteVersion + '_standalone_' + process.platform + '.zip';
         request.get({url: url, encoding: 'binary'}, (err, response, body) => {
             if (err) {
                 vscode.window.showErrorMessage(err);
-                this.println(err);
+                Logger.println(err);
             } else {
                 fs.writeFileSync(vscode.workspace.rootPath + "/luna.zip", body, 'binary');
 
                 extract_zip(vscode.workspace.rootPath + "/luna.zip", {dir: vscode.workspace.rootPath}, (err) => {
                     if (err) {
                         vscode.window.showErrorMessage("Could not update Luna to version " + remoteVersion);
-                        this.println("Could not update Luna to version " + remoteVersion + "\n");
+                        Logger.println("Could not update Luna to version " + remoteVersion + "\n");
                     } else {
                         fs.unlinkSync(vscode.workspace.rootPath + "/luna.zip");
-                        this.println("Luna was successfully updated!\n");
+                        Logger.println("Luna was successfully updated!\n");
                         vscode.workspace.getConfiguration('luna').update('version', remoteVersion, vscode.ConfigurationTarget.Workspace);
                     }
                 });
@@ -122,11 +116,6 @@ export default class LunaProject {
         request.get({url: 'https://raw.githubusercontent.com/XyronLabs/Luna/master/build/vscode_version'}, (err, response, body) => {
             _callback(body);
         });
-    }
-    
-    private println(text) {
-        this.dateTime.setTime(Date.now());
-        this.outputChannel.appendLine("[" + this.dateTime.toLocaleString() + "] " + text);
     }
 
     private initializeButtons(): void {
@@ -165,7 +154,7 @@ export default class LunaProject {
         let luna_update         = vscode.commands.registerCommand('luna.update',      () => this.checkForUpdates());
         let luna_force_update   = vscode.commands.registerCommand('luna.forceupdate', () => this.checkForUpdates(true));
         let luna_open_wiki      = vscode.commands.registerCommand('luna.open.wiki',   () => vscode.commands.executeCommand('vscode.open', vscode.Uri.parse("https://github.com/XyronLabs/Luna/wiki")));
-        let luna_open_output    = vscode.commands.registerCommand('luna.open.output', () => this.outputChannel.show());
+        let luna_open_output    = vscode.commands.registerCommand('luna.open.output', () => Logger.show());
 
         context.subscriptions.push(luna_run_current);
         context.subscriptions.push(luna_run_main);
