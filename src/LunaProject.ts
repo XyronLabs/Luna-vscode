bd7d7adcf35c1964365d36b8312c43e9346e2f65
ACharLuk <acharluk@gmail.com>
2018-02-06 19:31:21 +0100

    Logging works!

--- src/LunaProject.ts
+++ src/LunaProject.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as request from 'request';
 import * as extract_zip from 'extract-zip';
+import * as LunaManager from 'luna-manager';
 
 import LaunchHandler from './LaunchHandler';
 import ExtensionHandler from './ExtensionHandler';
@@ -16,7 +17,14 @@ export default class LunaProject {
     private launchHandler: LaunchHandler;
     private extensionHandler: ExtensionHandler;
 
+    private path: string;
+    private printfn: Function;
+
     constructor(context: vscode.ExtensionContext) {
+        Logger.println("Loading Luna extension")
+
+        this.path = vscode.workspace.rootPath;
+        this.printfn = msg => Logger.println(msg);
         
         this.registerCommands(context);
         this.initializeButtons();
@@ -25,7 +33,7 @@ export default class LunaProject {
     
         if (vscode.workspace.getConfiguration('luna').get('isLunaProject')
             && vscode.workspace.getConfiguration('luna').get('autoUpdateBinaries')) {
-            this.checkForUpdates(false, true);
+            LunaManager.checkForUpdates(this.path, this.printfn, false);
         }
 
         if (vscode.workspace.getConfiguration('luna').get('autoUpdateExtensions')) {
@@ -47,74 +55,14 @@ export default class LunaProject {
     }
 
     newProject(): void {
-        this.checkForUpdates(true);
-
+        LunaManager.newProject(this.path, this.printfn);
+        
         this.createSettings();
-
+        
         // Create main.lua and open it
         fs.appendFile(vscode.workspace.rootPath + '/main.luna','');
         vscode.workspace.openTextDocument(vscode.workspace.rootPath + '/main.luna').then(doc => {
             vscode.window.showTextDocument(doc);
-        });
-    }
-
-    checkForUpdates(force?: boolean, autoHide?: boolean): void {
-        Logger.show();
-        Logger.println("Luna is checking for updates, please wait...");
-        let currentVersion = this.checkCurrentBinariesVersion();
-        
-        this.checkRemoteBinariesVersion(remoteVersion => {
-            Logger.println("Current version: " + currentVersion);
-            Logger.println("Remote version: " + remoteVersion);
-    
-            if (!remoteVersion) {
-                Logger.println("Error fetching the latest version!");
-                return;
-            }
-    
-            if (!currentVersion || currentVersion < remoteVersion || force)
-                this.updateBinaries(remoteVersion);
-            else
-                Logger.println('Luna is up to date!\n');
-            
-            if (Logger.autoHideOutput && autoHide)
-                Logger.hide();
-        });
-    }
-
-    updateBinaries(remoteVersion: string): void {
-        Logger.println("Installing Luna " + remoteVersion + " to this folder: " + vscode.workspace.rootPath);
-        Logger.println("Please wait until this process is finished...")
-        
-        let url = 'https://github.com/XyronLabs/Luna/releases/download/' + remoteVersion + '/luna-' + remoteVersion + '_standalone_' + process.platform + '.zip';
-        request.get({url: url, encoding: 'binary'}, (err, response, body) => {
-            if (err) {
-                vscode.window.showErrorMessage(err);
-                Logger.println(err);
-            } else {
-                fs.writeFileSync(vscode.workspace.rootPath + "/luna.zip", body, 'binary');
-
-                extract_zip(vscode.workspace.rootPath + "/luna.zip", {dir: vscode.workspace.rootPath}, (err) => {
-                    if (err) {
-                        vscode.window.showErrorMessage("Could not update Luna to version " + remoteVersion);
-                        Logger.println("Could not update Luna to version " + remoteVersion + "\n");
-                    } else {
-                        fs.unlinkSync(vscode.workspace.rootPath + "/luna.zip");
-                        Logger.println("Luna was successfully updated!\n");
-                        vscode.workspace.getConfiguration('luna').update('version', remoteVersion, vscode.ConfigurationTarget.Workspace);
-                    }
-                });
-            }
-        });
-    }
-
-    private checkCurrentBinariesVersion(): string {
-        return vscode.workspace.getConfiguration('luna').get('version');
-    }
-
-    private checkRemoteBinariesVersion(_callback): void {
-        request.get({url: 'https://raw.githubusercontent.com/XyronLabs/Luna/master/build/vscode_version'}, (err, response, body) => {
-            _callback(body);
         });
     }
 
@@ -151,8 +99,8 @@ export default class LunaProject {
         let luna_run_current    = vscode.commands.registerCommand('luna.run.current', () => this.launch(vscode.window.activeTextEditor.document.fileName));
         let luna_run_main       = vscode.commands.registerCommand('luna.run.main',    () => this.launch("main.luna"));
         let luna_create_project = vscode.commands.registerCommand('luna.initproject', () => this.newProject());
-        let luna_update         = vscode.commands.registerCommand('luna.update',      () => this.checkForUpdates());
-        let luna_force_update   = vscode.commands.registerCommand('luna.forceupdate', () => this.checkForUpdates(true));
+        let luna_update         = vscode.commands.registerCommand('luna.update',      () => LunaManager.checkForUpdates(this.path, this.printfn));
+        let luna_force_update   = vscode.commands.registerCommand('luna.forceupdate', () => LunaManager.checkForUpdates(this.path, this.printfn, true));
         let luna_open_wiki      = vscode.commands.registerCommand('luna.open.wiki',   () => vscode.commands.executeCommand('vscode.open', vscode.Uri.parse("https://github.com/XyronLabs/Luna/wiki")));
         let luna_open_output    = vscode.commands.registerCommand('luna.open.output', () => Logger.show());
 
